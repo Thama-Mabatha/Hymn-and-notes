810055ca98d965e69ecad10201b28ea98a1d2050
Thama Mabatha <thama.mabatha@gmail.com>
2026-09-02 23:26:10 +0200

    database changes

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,11 @@ import react from '@vitejs/plugin-react';
 import { VitePWA } from 'vite-plugin-pwa';
 
 export default defineConfig({
+  css: {
+    postcss: {
+      plugins: []
+    }
+  },
   plugins: [
     react(),
     VitePWA({
